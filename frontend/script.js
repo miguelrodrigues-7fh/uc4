a014781d7273360ce23d222f46670e72936822a0
miguelrodrigues-7fh <moraomiguel54@gmail.com>
2026-09-09 17:01:51 -0300

    att

--- frontend/script.js
+++ frontend/script.js
@@ -1,10 +1,27 @@
-
-
-fetch('http://localhost:3000/api/mensagem')
-    .then(response => response.json())
-    .then(data => {
         const msn = document.getElementById('sla');
         const orkut = document.getElementById('giratina');
-        orkut.innerHTML = (data.sub);
-        msn.innerHTML = (data.mensagem);
-    });
+
+
+
+async function dataConsumo() {
+        try { 
+            const resposta = await fetch('http://localhost:3000/api/mensagem');
+            if (!resposta.ok) { 
+                throw new Error(`Erro HTTP: ${resposta.status}`);
+            }
+           const data = await resposta.json();
+            //const titulo = document.getElementById('sla')
+           // titulo.innerHTML = data.mensagem;
+           // const descri = document.getAnimations('giratina');
+           // descri.innerHTML = data.description;
+            orkut.innerHTML = (data.sub);
+            msn.innerHTML = (data.mensagem);
+        }
+            catch (erro) { 
+                console.error('Error: ', erro);
+            }
+    };
+     dataConsumo();
+
+    
+
